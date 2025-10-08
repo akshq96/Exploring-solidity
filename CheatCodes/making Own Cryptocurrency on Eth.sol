9687d89d052c3dfb6baabe9c2c66ee75e6bd48a6
akshq96 <akshitraj19@gmail.com>
2025-10-08 19:01:47 +0530

    g

---
+++ CheatCodes/making Own Cryptocurrency on Eth.sol
@@ -0,0 +1,14 @@
+// created a new contract file, I named mine LW3Token.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+// contract LW3Token is ERC20 {
+//     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+//         _mint(msg.sender, 10 * 10 ** 18);
+//     }
+// }
+
+// This code is also written in LW3Token.sol file.
